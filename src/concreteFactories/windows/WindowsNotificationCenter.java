60db3658d471e52b375ba35a68fdb84e0a8a44d1
VictorCanovas <alu0101221953@ull.edu.es>
2024-11-13 16:46:22 +0000

    Modificacion de la clase WindowsNotificationCenter. Ahora solo funciona en windows.

--- src/concreteFactories/windows/WindowsNotificationCenter.java
+++ src/concreteFactories/windows/WindowsNotificationCenter.java
@@ -1,46 +1,49 @@
 package concreteFactories.windows;
 
 import abstractFactory.NotificationCenter;
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.win32.W32APIOptions;
 import oshi.SystemInfo;
 import oshi.hardware.CentralProcessor;
 import oshi.hardware.GlobalMemory;
 import oshi.software.os.OperatingSystem;
 
-import java.awt.*;
-import java.awt.TrayIcon.MessageType;
-
 public class WindowsNotificationCenter implements NotificationCenter {
+
+    // Definimos el valor de MB_ICONINFORMATION manualmente
+    private static final int MB_ICONINFORMATION = 0x40;
+
+    // Interfaz para cargar la biblioteca User32 de Windows mediante JNA
+    public interface User32 extends Library {
+        User32 INSTANCE = Native.load("user32", User32.class, W32APIOptions.DEFAULT_OPTIONS);
+
+        // Función para mostrar un mensaje (MessageBox)
+        int MessageBox(Pointer hWnd, String lpText, String lpCaption, int uType);
+    }
 
     @Override
     public void showNotification(String title, String message) {
-        if (SystemTray.isSupported()) {
-            SystemTray tray = SystemTray.getSystemTray();
-            Image image = Toolkit.getDefaultToolkit().createImage("img/icon.png");
-            TrayIcon trayIcon = new TrayIcon(image, "Tray Demo");
-            trayIcon.setImageAutoSize(true);
-            trayIcon.setToolTip("System tray icon demo");
-
-            try {
-                tray.add(trayIcon);
-                trayIcon.displayMessage(title, message, MessageType.INFO);
-                System.out.println("Notification displayed: " + title + " - " + message);
-            } catch (AWTException e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.err.println("System tray not supported!");
-        }
+        // Utilizamos la función MessageBox de User32 para mostrar la notificación en Windows
+        User32.INSTANCE.MessageBox(null, message, title, MB_ICONINFORMATION);
+        System.out.println("Notification displayed (Windows-specific): " + title + " - " + message);
     }
 
     public void showSystemInfoNotification() {
+        // Obtiene información del sistema (con OSHI) y la muestra en una notificación
+        String systemInfoMessage = getSystemInfo();
+        showNotification("System Information", systemInfoMessage);
+    }
+
+    private String getSystemInfo() {
+        // Metodo auxiliar para obtener la información del sistema con OSHI
         SystemInfo systemInfo = new SystemInfo();
         OperatingSystem os = systemInfo.getOperatingSystem();
         CentralProcessor processor = systemInfo.getHardware().getProcessor();
         GlobalMemory memory = systemInfo.getHardware().getMemory();
 
-        String systemInfoMessage = String.format("OS: %s\nCPU: %s\nMemory: %d bytes",
-                os, processor, memory.getTotal());
-
-        showNotification("System Information", systemInfoMessage);
+        return String.format("OS: %s\nCPU: %s\nMemory: %.2f GB",
+                os, processor, memory.getTotal() / (1024.0 * 1024 * 1024));
     }
 }
