733bb44f7a3978f38cd2d60af254eff4208fe90a
Victor Canovas <72502501+alu0101221953@users.noreply.github.com>
2024-11-14 19:07:28 +0000

    Delete src/Main.java

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import concreteFactories.windows.*;
-
-public class Main {
-    public static void main(String[] args) {
-        WindowsMenu windowsMenu = new WindowsMenu();
-        windowsMenu.render();
-    }
-}
